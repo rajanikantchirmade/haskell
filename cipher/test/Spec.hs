9362f0f914a27faf6ad8a37f62ba38177a3dfc65
Rajanikant Chirmade <rajanikant.chirmade@seagate.com>
2018-06-19 04:00:26 -0700

    Test suit using Hspec
    - Add test for verifiction of encode/decode functions.

--- cipher/test/Spec.hs
+++ cipher/test/Spec.hs
@@ -1,2 +1,15 @@
+
+import Test.Hspec
+import Cipher
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "encode" $ do
+    it "encode (string key) == encoded string" $do
+      encode' "Hello World" "pqr" `shouldBe` "Wucae Leiat"
+  describe "decode" $ do
+    it "decode (string key) == decoded string" $do
+      decode' "Wucae Leiat" "pqr" `shouldBe` "Hello World"
+  describe "encode/decode" $ do
+    it "string == decode (encode (string key) key)" $ do
+      "Hello World" `shouldBe` decode' (encode' "Hello World" "xyz") "xyz"
