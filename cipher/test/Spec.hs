1448f786d1d843478c893de3815d4d4b4accc8e3
Rajanikant Chirmade <rajanikant.chirmade@seagate.com>
2018-06-19 04:17:38 -0700

    QuickCheck test
    - Add QuickCheck

--- cipher/test/Spec.hs
+++ cipher/test/Spec.hs
@@ -1,15 +1,21 @@
 
 import Test.Hspec
+import Test.QuickCheck
 import Cipher
 
+encode_decode :: [Char] -> [Char] -> Bool
+encode_decode s k = s == decode' (encode' s k) k
+
 main :: IO ()
-main = hspec $ do
-  describe "encode" $ do
-    it "encode (string key) == encoded string" $do
-      encode' "Hello World" "pqr" `shouldBe` "Wucae Leiat"
-  describe "decode" $ do
-    it "decode (string key) == decoded string" $do
-      decode' "Wucae Leiat" "pqr" `shouldBe` "Hello World"
-  describe "encode/decode" $ do
-    it "string == decode (encode (string key) key)" $ do
-      "Hello World" `shouldBe` decode' (encode' "Hello World" "xyz") "xyz"
+main =  do
+  quickCheck encode_decode 
+  hspec $ do
+    describe "encode" $ do
+      it "encode (string key) == encoded string" $do
+        encode' "Hello World" "pqr" `shouldBe` "Wucae Leiat"
+    describe "decode" $ do
+      it "decode (string key) == decoded string" $do
+        decode' "Wucae Leiat" "pqr" `shouldBe` "Hello World"
+    describe "encode/decode" $ do
+      it "string == decode (encode (string key) key)" $ do
+        "Hello World" `shouldBe` decode' (encode' "Hello World" "xyz") "xyz"
