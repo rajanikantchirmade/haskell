44622b89a1a99364a97b43a184c33f78abf8eaae
Rajanikant Chirmade <rajanikant.chirmade@seagate.com>
2018-06-20 09:34:22 -0700

    Quickcheck property added with string generator

--- cipher/test/Spec.hs
+++ cipher/test/Spec.hs
@@ -1,10 +1,22 @@
 
 import Test.Hspec
 import Test.QuickCheck
+import Test.QuickCheck.Property
 import Cipher
 
-encode_decode :: String -> String -> Bool
-encode_decode s k = s == decode' (encode' s k) k
+stringGen :: Gen String
+stringGen = listOf $ elements ['a'..'z']
+
+tupleGen :: Gen (String, String)
+tupleGen = do
+  a <- stringGen
+  b <- stringGen
+  return (a, b)
+
+prop_encode_decode :: Property
+prop_encode_decode =
+  forAll tupleGen
+  (\(s, k) -> (s ==  decode' (encode' s k) k))
 
 main :: IO ()
 main =  do
@@ -18,4 +30,4 @@ main =  do
     describe "encode/decode" $ do
       it "string == decode (encode (string key) key)" $ do
         "Hello World" `shouldBe` decode' (encode' "Hello World" "xyz") "xyz"
-  verboseCheck encode_decode 
+  verboseCheck prop_encode_decode 
